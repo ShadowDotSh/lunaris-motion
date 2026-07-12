ee3b82e739e1b965bc7bdce5c567ad1bc9e292f7
ShadowDotSh <300275593+ShadowDotSh@users.noreply.github.com>
2026-07-12 01:49:40 -0300

    feat(player): add mpv config

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ case "$1" in
     sudo install -Dm755 "./src/lunaris-motion"                "/usr/bin/lunaris-motion"
     sudo install -Dm644 "./src/etc/lunaris-motion-list.rasi"  "$target/lunaris-motion-list.rasi"
     sudo install -Dm644 "./src/etc/lunaris-motion-grid.rasi"  "$target/lunaris-motion-grid.rasi"
+    sudo install -Dm644 "./src/etc/mpv.conf"  "$target/mpv.conf"
   ;;
 
   uninstall)
