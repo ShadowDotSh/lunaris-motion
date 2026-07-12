0d64f5a411e8f41de779344a8749be0e7ca9de3f
ShadowDotSh <300275593+ShadowDotSh@users.noreply.github.com>
2026-07-12 01:49:40 -0300

    feat(build): add installation script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+target="/etc/xdg/lunaris-motion"
+
+case "$1" in
+  install)
+    sudo install -Dm755 "./src/lunaris-motion"                "/usr/bin/lunaris-motion"
+    sudo install -Dm644 "./src/etc/lunaris-motion-list.rasi"  "$target/lunaris-motion-list.rasi"
+    sudo install -Dm644 "./src/etc/lunaris-motion-grid.rasi"  "$target/lunaris-motion-grid.rasi"
+  ;;
+
+  uninstall)
+    sudo rm -rf "/usr/bin/lunaris-motion"
+    sudo rm -rf "$target"
+  ;;
+
+  *)
+    echo "Usage: $0 {install|uninstall}"
+  ;;
+esac
